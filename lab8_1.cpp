067375fa232e9b5981bca22cca9c1de2d601052e
WizAom <Aom_ll2555@hotmail.com>
2020-01-24 16:10:47 +0700

    1st

--- lab8_1.cpp
+++ lab8_1.cpp
@@ -3,10 +3,14 @@
 using namespace std;
 
 int main(){	
-
-	//use 'setw' to set width of table and 'left' to set left-alignment
-	//you can change input argument of 'setw()' to see the effect
-	//Try to change from 'left' to 'right' and see the effect
+	double loan,rate,pay,payment,total,dok,newba;
+	int i=0;
+	cout << "Enter intitial loan: ";
+	cin >> loan;
+	cout << "Enter interest rate per year (%): ";
+	cin >> rate;
+	cout << "Enter amount you can pay per year: ";
+	cin >> pay;
 	cout << setw(13) << left << "EndOfYear#"; 
 	cout << setw(13) << left << "PrevBalance"; 
 	cout << setw(13) << left << "Interest"; 
@@ -14,17 +18,35 @@ int main(){
 	cout << setw(13) << left << "Payment";
 	cout << setw(13) << left << "NewBalance";
 	cout << "\n";
+	while(loan!=0){
+		dok=loan*(rate/100);
+		total=loan+dok;
+		if(total<pay){
+				pay=total;
+				newba=0;
+		}else{
+			newba=total-pay;
+		}
+		i++;
+		cout << fixed << setprecision(2); 
+		cout << setw(13) << left << i; 
+		cout << setw(13) << left << loan;
+		cout << setw(13) << left << dok;
+		cout << setw(13) << left << total;
+		cout << setw(13) << left << pay;
+		cout << setw(13) << left << newba;
+		cout << "\n";	
+		loan=newba;
+
+	}
+	
+
+	//use 'setw' to set width of table and 'left' to set left-alignment
+	//you can change input argument of 'setw()' to see the effect
+	//Try to change from 'left' to 'right' and see the effect
 	
 	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
 	//you can change input argument of 'setprecision()' to see the effect
-	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
-	cout << "\n";	
 	
 	return 0;
 }
--- lab8_2.cpp
+++ lab8_2.cpp
@@ -1,7 +1,18 @@
 #include<iostream>
 using namespace std;
-
-//Write the function printO() here
+int printO(int x,int y){
+	if(x<=0 || y<=0){
+		cout << "Invalid input\n";
+	}else{
+		for(int i=1;i<=x;i++){
+			for(int n=1;n<=y;n++){
+			cout << "O";
+			}
+		cout << "\n";
+		}
+	}
+	
+}
 
 int main(){
 	
